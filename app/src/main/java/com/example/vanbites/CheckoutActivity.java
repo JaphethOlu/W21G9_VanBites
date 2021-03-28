b7a2ae89406cad2ee0f692acdeaeb39fb569f194
JiljoM <jiljomathew93@gmail.com>
2021-03-27 18:57:56 -0700

    cart 2 commit

--- app/src/main/java/com/example/vanbites/CheckoutActivity.java
+++ app/src/main/java/com/example/vanbites/CheckoutActivity.java
@@ -3,6 +3,10 @@ package com.example.vanbites;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        EditText editDelivery=findViewById(R.id.etDelivery);
+        TextView txtAddress=findViewById(R.id.txtAddress);
+        Spinner spinnerPayment=findViewById(R.id.spinnerPayment);
+        Button btnPlaceOrder=findViewById(R.id.btnPlaceOrder);
+
+
+        Address address= (Address) getIntent().getParcelableExtra("address");
+        String adressFinal=address.getAddress();
+
+        txtAddress.setText(adressFinal);
+
+
     }
 }
