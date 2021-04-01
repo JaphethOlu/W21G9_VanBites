ff7e685090d89c2530770a8bedda4f87860ded65
volkov <volkov0kostya@gmail.com>
2021-04-01 10:40:57 -0700

    order placed activity

---
+++ app/src/main/java/com/example/vanbites/OrderPlacedActivity.java
@@ -0,0 +1,16 @@
+package com.example.vanbites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderPlacedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_placed);
+
+
+    }
+}
